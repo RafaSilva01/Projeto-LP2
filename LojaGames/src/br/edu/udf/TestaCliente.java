8dc98761185809b06e5cf8722472059d6e2a9159
Decael <decaelgomes@gmail.com>
2020-05-27 18:00:21 -0300

    Melhorias nas Classes: TestaCliente e TestaProduto.
    Signed-off-by: Decael <decaelgomes@gmail.com>

--- LojaGames/src/br/edu/udf/TestaCliente.java
+++ LojaGames/src/br/edu/udf/TestaCliente.java
@@ -5,13 +5,15 @@ public class TestaCliente {
         DBClientes dbClientes = new DBClientes();
 
         //Novo cliente criado
-        Cliente cliente = new Cliente("Joss", "70150465122", "acula", "decaelgomes@gmail.com", 5000.00);
+        Cliente cliente = new Cliente("Joss", "70150465122", "Rua intrincada", "joss.lang@gmail.com", 5000.00);
+        Cliente cliente1 = new Cliente("Arthus", "14253639870", "Bairro Bonito", "R.Arthusgomes@gmail.com", 10000.00);
+        Cliente cliente2 = new Cliente("Thurion", "82520465213", "Avenida Vailá", "T.gusthus@gmail.com", 10.00);
 
         //Cadastra Cliente
         dbClientes.cadastrarCliente(cliente);
         dbClientes.listaClientes();
 
-        //altera Cliente
+        //Editar Cliente
         Cliente cliente1 = new Cliente("Algoz", "70150165122", "asdfisdf", "d@mail.com", 10000.00);
         dbClientes.editarCliente(cliente);
 
--- LojaGames/src/br/edu/udf/TestaProduto.java
+++ LojaGames/src/br/edu/udf/TestaProduto.java
@@ -15,13 +15,31 @@ public class TestaProduto {
         produto.setPrecoOriginal(50.01);
         produto.aumentarQuantidade(10);
 
+        //Novo produto criado 2
+        Produto produto1 = new Produto();
+        produto1.setNome("The Last of us");
+        produto1.setCodigo("004");
+        produto1.setPrecoOriginal(10.20);
+        produto1.aumentarQuantidade(5);
+
+        //Novo produto criado 3
+        Produto produto2 = new Produto();
+        produto2.setNome("Destiny");
+        produto2.setCodigo("010");
+        produto2.setPrecoOriginal(80.03);
+        produto2.aumentarQuantidade(7);
+
+
+
         //Cadastra produto Produto
         dbProdutos.cadastrarProduto(produto);
+        dbProdutos.cadastrarProduto(produto1);
+        dbProdutos.cadastrarProduto(produto2);
         dbProdutos.listaProdutos();
 
         //altera Produto
-        Produto produto1 = new Produto("1234", "Call of Duty: Modern Warfare", 100.02);
-        dbProdutos.editarProduto(produto);
+        //Produto produto1 = new Produto("1234", "Call of Duty: Modern Warfare", 100.02);
+        //dbProdutos.editarProduto(produto);
 
         //Excluir
         System.out.println(dbProdutos.excluirProduto("123"));
